a0a7dbb75e271af2e203a86bd9a8c393afcfbd94
aungphyoeko <ophyokaung@gmail.com>
2016-07-08 10:30:22 +0630

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,25 @@
 <?php
-include('cookie.inc');
+require 'page_processing.inc';
+require 'cookie.inc';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
 <head>
 	<title>Student Online Quiz</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<?php include 'import.inc'; ?>
 </head>
 <body>
-<?php
-$show_login_page = true;
-$show_dashboard_page = false;
-?>
 
 <?php
-if(isset($_POST['loginformsubmitted'])):
-	include("login_process.inc");
-	if($is_login_approved==true):
-		$show_dashboard_page=true;
-		$show_login_page=false;
-	endif;
-endif;
-
 if($show_login_page):
-	include("login.inc");
+	include 'login.inc';
 elseif($show_dashboard_page):
-	include("dashboard.inc");
+	include 'dashboard.inc';
+elseif($show_profile_page):
+	include 'profile.inc';
 endif;
 
 ?>
+
 </body>
 </html>
