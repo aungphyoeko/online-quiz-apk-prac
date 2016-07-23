c462392302fadc863e23c916032c8e0f4d320d07
aungphyoeko <ophyokaung@gmail.com>
2016-07-23 14:06:33 +0630

    Add files via upload

--- database/students_login_database.sql
+++ database/students_login_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2016 at 11:26 AM
+-- Generation Time: Jul 23, 2016 at 09:34 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -19,8 +19,116 @@ SET time_zone = "+00:00";
 --
 -- Database: `online-quiz`
 --
-CREATE DATABASE IF NOT EXISTS `online-quiz` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `online-quiz`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer_list`
+--
+
+CREATE TABLE `answer_list` (
+  `answer_id` int(11) NOT NULL,
+  `answer_msg` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `answer_list`
+--
+
+INSERT INTO `answer_list` (`answer_id`, `answer_msg`) VALUES
+(1, 'option 1'),
+(2, 'option 2'),
+(3, 'option 3'),
+(4, 'option 4'),
+(5, 'option 6'),
+(7, 'option 8'),
+(9, 'option 9'),
+(10, 'option 10'),
+(11, 'option 11'),
+(12, 'option 12'),
+(13, 'option 13'),
+(14, 'option 14'),
+(15, 'option 15'),
+(16, 'option 16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `answer_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`answer_id`, `question_id`) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 2),
+(6, 3),
+(7, 3),
+(8, 3),
+(9, 4),
+(10, 4),
+(11, 5),
+(12, 5),
+(13, 6),
+(14, 6),
+(15, 1),
+(16, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_list`
+--
+
+CREATE TABLE `question_list` (
+  `question_id` int(11) NOT NULL,
+  `question_msg` text NOT NULL,
+  `answer_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `question_list`
+--
+
+INSERT INTO `question_list` (`question_id`, `question_msg`, `answer_id`, `quiz_id`) VALUES
+(1, 'This is a question. So, answer.', 1, 1),
+(2, 'this is another question ?', 2, 1),
+(3, 'some meaning-less quesiton?', 3, 1),
+(4, 'bla bla bla lba bla bla ?', 4, 1),
+(5, 'this is for quiz 2?', 5, 2),
+(6, 'another question or quiz 2 angain?', 6, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_list`
+--
+
+CREATE TABLE `quiz_list` (
+  `quiz_id` int(11) NOT NULL,
+  `quiz_name` varchar(25) NOT NULL,
+  `quiz_level` int(11) NOT NULL,
+  `leacture_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quiz_list`
+--
+
+INSERT INTO `quiz_list` (`quiz_id`, `quiz_name`, `quiz_level`, `leacture_id`) VALUES
+(1, 'Quiz 1', 2, 1),
+(2, 'Quiz 2', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -43,11 +151,38 @@ CREATE TABLE `students_login` (
 --
 
 INSERT INTO `students_login` (`id`, `name`, `email`, `pass`, `dob`, `pref`, `address`) VALUES
-(1, 'Aung', 'test@gmail.com', 'pass', '2016-07-01', 'mr', 'Mandalay');
+(1, 'Aung', 'test@gmail.com', 'pass', '1992-11-01', 'mr', 'Mandalay');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `answer_list`
+--
+ALTER TABLE `answer_list`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD UNIQUE KEY `answer_id` (`answer_id`);
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`answer_id`,`question_id`);
+
+--
+-- Indexes for table `question_list`
+--
+ALTER TABLE `question_list`
+  ADD PRIMARY KEY (`question_id`),
+  ADD UNIQUE KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `quiz_list`
+--
+ALTER TABLE `quiz_list`
+  ADD PRIMARY KEY (`quiz_id`),
+  ADD UNIQUE KEY `quiz_id` (`quiz_id`);
 
 --
 -- Indexes for table `students_login`
@@ -56,6 +191,30 @@ ALTER TABLE `students_login`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answer_list`
+--
+ALTER TABLE `answer_list`
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `question_list`
+--
+ALTER TABLE `question_list`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `quiz_list`
+--
+ALTER TABLE `quiz_list`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `students_login`
+--
+ALTER TABLE `students_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
